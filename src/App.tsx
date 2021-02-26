0a6c6f3f7eff8f2fe2942f545dced9bfdfd5b287
ashishcentrica <ashish.patil@centrica.com>
2021-02-26 19:35:24 +0000

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import "./App.css";
-import Chessboard from "chessboardjsx";
-import { ChessInstance, ShortMove } from "chess.js";
-
-const Chess = require("chess.js");
+import {ToastProvider} from "react-toast-notifications";
+import {MainChess} from "./MainChess";
 
 const App: React.FC = () => {
-
-  const [chess, setChess] = useState<ChessInstance>(
-    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-  );
-  const [fen, setFen] = useState(chess.fen());
-
-  useEffect(() => {
-    console.log('chess called',chess);
-  }, [chess, fen]);
-
-
-  const handleMove = (move: ShortMove) => {
-    if (chess.move(move)) {
-      setTimeout(() => {
-        const moves = chess.moves();
-
-        if (moves.length > 0) {
-          const computerMove = moves[Math.floor(Math.random() * moves.length)];
-          chess.move(computerMove);
-          setFen(chess.fen());
-        }
-      }, 300);
-
-      setFen(chess.fen());
-    }
-  };
-
-  const handleClear = () => {
-    console.log('called this one',);
-    setChess(chess)
-    // chess.clear();
-    setFen(chess.fen());
-  };
-
   return (
-    <div className="container">
-      <h1>Random Chess</h1>
-
-      <Chessboard
-        width={900}
-        position={fen}
-        onDrop={(move) =>
-          handleMove({
-            from: move.sourceSquare,
-            to: move.targetSquare,
-            promotion: "q",
-          })
-        }
-      />
-      <button type="submit" onClick={handleClear} className="btn btn__primary btn__lg">
-        Clear
-      </button>
-    </div>
+    <ToastProvider placement={'top-right'} autoDismiss={true} autoDismissTimeout={10000} >
+      <MainChess />
+    </ToastProvider>
   );
 };
 
---
+++ src/MainChess.tsx
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from "react";
+import Chessboard from "chessboardjsx";
+import {ChessInstance, ShortMove} from "chess.js";
+import {useToasts} from "react-toast-notifications";
+
+export const MainChess = () => {
+  const Chess = require("chess.js");
+  const [chess, setChess] = useState<ChessInstance>(new Chess());
+  const [fen, setFen] = useState(chess.fen());
+  const {addToast} = useToasts()
+
+
+  useEffect(() => {
+    if (chess.game_over()) {
+      addToast('Game Over', {
+        appearance: 'error',
+        autoDismiss: false,
+      })
+    }
+    if (chess.in_stalemate()) {
+      addToast('Stalemate', {
+        appearance: 'error',
+        autoDismiss: false,
+      })
+    }
+
+    if(chess.in_check()) {
+      addToast('You have a Check!!', {
+        appearance: 'error',
+        autoDismiss: false,
+      })
+    }
+  }, [chess, fen]);
+
+
+  const handleMove = (move: ShortMove) => {
+    if (chess.move(move)) {
+      setTimeout(() => {
+        const moves = chess.moves();
+
+        if (moves.length > 0) {
+          const computerMove = moves[Math.floor(Math.random() * moves.length)];
+          chess.move(computerMove);
+          setFen(chess.fen());
+        }
+      }, 300);
+
+      setFen(chess.fen());
+    }
+  };
+
+  const handleClear = () => {
+    setChess(new Chess())
+    setFen(chess.fen());
+  };
+  {
+    return <div
+      className="container">
+      <h1>Random Chess</h1>
+
+      <Chessboard
+        width={900}                                                K
+        position={fen}
+        onDrop={(move: any) =>
+          handleMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            promotion: "q",
+          })}
+      />
+      <button type="submit" onClick={handleClear} className="btn btn__primary btn__lg">
+        Clear
+      </button>
+    </div>;
+  }
+}
